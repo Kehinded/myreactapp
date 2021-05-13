78fb377dbc7119a2642928018eba74cb2538cca8
Kehinded <ayenikehinded@gmail.com>
2021-05-13 14:33:27 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import logo from './images/logo.svg';
+import mockup from "./images/illustration-mockups.svg"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <img src = {logo} alt = "images"></img>
       </header>
-    </div>
+
+      <main>
+      
+      <img src = {mockup} alt = "images" ></img>
+
+      <div className = "content">
+        <h2>Build the community your fans will love</h2>
+        <p>lakadlh jgdbwjvwfdh habhfyvhwv hafw ghvdwwfwwjdjh fxvgdtgd gadsdw hffssvdw thdsvwvdd fhsjag</p>
+        <button>register</button>
+      </div>
+
+      </main>
+
+      <footer>
+      <i class="fab fa-facebook-f fa-2x"></i>
+      <i class="fab fa-whatsapp fa-2x"></i>
+      <i class="fab fa-twitter fa-2x"></i>
+      <i class="fab fa-instagram fa-2x"></i>
+      </footer>
+    </>
   );
 }
 
